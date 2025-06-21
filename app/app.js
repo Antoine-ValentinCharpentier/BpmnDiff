bbed0730094b11a8d7e4619250e1bac18ede81b1
Antoine-ValentinCharpentier <antoinevalentin.charpentier@gmail.com>
2025-06-21 19:47:17 +0200

    :fire: remove database

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bpmnRoutes from './routes/bpmn.routes.js';
-import sequelize from './config/db.js';
 
 const PORT = 3000;
 
@@ -8,8 +7,6 @@ const app = express();
 
 app.use(express.json());
 app.use('/', bpmnRoutes);
-
-await sequelize.sync();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- app/services/bpmn.service.js
+++ app/services/bpmn.service.js
@@ -2,10 +2,10 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { v4 as uuidv4 } from 'uuid';
-import BpmnFile from '../models/bpmn.model.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const uploadDir = path.join(__dirname, '../../uploads');
+fs.mkdir(uploadDir, { recursive: true });
 
 export const saveBpmnFiles = async (bpmnBefore, bpmnAfter) => {
   const id = uuidv4();
@@ -15,8 +15,6 @@ export const saveBpmnFiles = async (bpmnBefore, bpmnAfter) => {
 
   await fs.writeFile(path.join(uploadDir, filenameBefore), bpmnBefore.buffer);
   await fs.writeFile(path.join(uploadDir, filenameAfter), bpmnAfter.buffer);
-
-  await BpmnFile.create({ id, nameBpmnBefore: filenameAfter, nameBpmnAfter: filenameAfter });
 
   return `/diff/${id}`;
 };
