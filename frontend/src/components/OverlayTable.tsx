03fa875aeb9fec4c40d5b5d73d85012dd40c3432
Antoine-Valentin <antoinevalentin.charpentier@gmail.com>
2025-09-10 21:47:45 +0200

    :sparkles: display changes details

--- frontend/src/components/OverlayTable.tsx
+++ frontend/src/components/OverlayTable.tsx
@@ -1,11 +1,34 @@
 import type { ChangedElement } from "bpmn-js-differ";
 
 export function OverlayTable({ diff }: { diff: ChangedElement }) {
+  if (!diff || !diff.attrs || Object.keys(diff.attrs).length === 0) {
+    return (
+      <div>
+        <p>Les modifications d'attributs n'ont pas pu être identifiées. Veuillez les consulter dans le modeler.</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <p>before</p>
-      {JSON.stringify(diff)}
-      <p>after</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Attribute</th>
+            <th>Old</th>
+            <th>New</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(diff.attrs).map(([attr, ch]) => (
+            <tr key={attr}>
+              <td>{attr}</td>
+              <td>{ch.oldValue}</td>
+              <td>{ch.newValue}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
