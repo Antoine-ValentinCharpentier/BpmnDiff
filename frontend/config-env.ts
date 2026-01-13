5c870ae18b2e38de84dfc949715d7e080ae0cd49
Antoine-Valentin <antoinevalentin.charpentier@gmail.com>
2026-01-13 23:35:58 +0100

    :bug: config env nullish variables

--- frontend/config-env.ts
+++ frontend/config-env.ts
@@ -2,11 +2,11 @@ const env = window.__ENV__ ?? {}
 
 export const config = {
   keycloak: {
-    url: env.KEYCLOAK_URL ?? 'http://localhost:18080/',
-    realm: env.KEYCLOAK_REALM ?? 'bpmn-diff',
-    clientId: env.KEYCLOAK_CLIENT_ID ?? 'frontend-react',
+    url: env.KEYCLOAK_URL || 'http://localhost:18080/',
+    realm: env.KEYCLOAK_REALM || 'bpmn-diff',
+    clientId: env.KEYCLOAK_CLIENT_ID || 'frontend-react',
   },
   backend: {
-    url: env.BACKEND_URL ?? 'http://localhost:9000/',
+    url: env.BACKEND_URL || 'http://localhost:9000/',
   },
 }
